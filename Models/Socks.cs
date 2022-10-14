4b9aa7bff1ea5bbcfa7aaad46b478fdbfde05b1b
jaylu <jaylunagariya7903@gmail.com>
2022-10-14 13:05:10 -0400

    Readme and Socks.cs

---
+++ Models/Socks.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace MvcSocks.Models
+{
+    public class Socks
+    {
+        public int Id { get; set; }
+        public string Company { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime LaunchDate { get; set; }
+        public string Type { get; set; }
+        public string Color { get; set; }
+        public string Size { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ README.txt
@@ -0,0 +1,14 @@
+﻿Author: Jaykumar Lunagariya
+Date: 2022/10/14
+
+12:45 I have started assignment upon Socks.
+	  I have created a web app which name is Socks.
+	  I created Socks.cs page.
+	  I included seven properties over there.
+
+12:59 I thought I'm missing something, and I found that
+	  it was readme file. 
+	  I created it.
+	  Then I upload this whole thing with github,
+	  and I commited and pushed it there
+	  on 1:04.
